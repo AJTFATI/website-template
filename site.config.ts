153abeee7af8c131e1f8a6b0ea9e70a274554229
AJTFATI <TAYLORALEXANDERJORDAN@GMAIL.COM>
2025-05-28 21:48:59 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,28 +1,18 @@
 import { siteConfig } from './lib/site-config'
 
-export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+export default {
+  rootNotionPageId: 'XXXXXXXXXXXX', // we'll get this in Step 3
+  name: 'Alexander J. Taylor',
+  domain: 'alexanderjtaylor.com',
+  author: 'Alexander J. Taylor',
+  github: 'ajtfati',
+  linkedin: 'alexanderjtaylor',
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  socialImageTitle: 'Alexander J. Taylor',
+  socialImageSubtitle: 'Digital Strategist | Storyteller',
+}
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
-
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
-
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
-
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
