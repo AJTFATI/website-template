20a7abfad740e495bfcac6e0e66fbac18972895e
AJTFATI <TAYLORALEXANDERJORDAN@GMAIL.COM>
2025-05-28 22:09:55 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -4,62 +4,56 @@ export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: '7875426197cf461698809def95960ebf',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
+
+export default {
+  rootNotionPageId: 'a557d4b9a1044e90b3e96d3dce10fc5a', // Home page
+
   rootNotionSpaceId: null,
 
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Alexander J. Taylor',
+  domain: 'alexanderjtaylor.com',
+  author: 'Alexander J. Taylor',
+  description:
+    'Digital strategist and creative storyteller specializing in media, PR, and identity. Powered by clarity, built for impact.',
 
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  github: 'ajtfati',
+  linkedin: 'alexanderjtaylor',
+  instagram: 'alexanderjtaylor',
+  twitter: null,
+  newsletter: null,
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
-
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
   isRedisEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/about': 'c9d8729f6e6747a2b92bd370aa1cf84e',
+    '/blog': '90328a1ad2d948cc84f181ef2545942e',
+    '/resume': '6e9a4c5f9aa54350909b77006ec94899',
+    '/contact': 'b0b4bb95b5e54f4087efc4a49897f1ee'
+  },
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
-})
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'c9d8729f6e6747a2b92bd370aa1cf84e'
+    },
+    {
+      title: 'Blog',
+      pageId: '90328a1ad2d948cc84f181ef2545942e'
+    },
+    {
+      title: 'Resume',
+      pageId: '6e9a4c5f9aa54350909b77006ec94899'
+    },
+    {
+      title: 'Contact',
+      pageId: 'b0b4bb95b5e54f4087efc4a49897f1ee'
+    }
+  ]
+}
+
