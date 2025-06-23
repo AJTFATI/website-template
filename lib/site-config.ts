b3d6ef587f5584d4d9d8656c20209e121288a325
AJTFATI <TAYLORALEXANDERJORDAN@GMAIL.COM>
2025-06-23 17:28:38 -0400

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -13,6 +13,8 @@ export interface SiteConfig {
   twitter?: string
   github?: string
   linkedin?: string
+  Instagram?: string
+  tiktok?: string
   newsletter?: string
   youtube?: string
   zhihu?: string
