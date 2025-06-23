eecf6cccf4dbbbe13cf22731000301b1d3c977b9
AJTFATI <TAYLORALEXANDERJORDAN@GMAIL.COM>
2025-06-23 18:54:24 -0400

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -13,46 +13,13 @@ interface SocialLink {
 }
 
 const socialLinks: SocialLink[] = [
-  config.twitter && {
-    name: 'twitter',
-    href: `https://twitter.com/${config.twitter}`,
-    title: `Twitter @${config.twitter}`,
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M23.44 4.83c-...' />
-      </svg>
-    )
-  },
-
-  config.github && {
-    name: 'github',
-    href: `https://github.com/${config.github}`,
-    title: `GitHub @${config.github}`,
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M9 19c-...' />
-      </svg>
-    )
-  },
-
   config.linkedin && {
     name: 'linkedin',
     href: `https://www.linkedin.com/in/${config.linkedin}`,
     title: `LinkedIn ${config.author}`,
     icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M6.5 21.5h-5v-13h5v13zM4...' />
-      </svg>
-    )
-  },
-
-  config.newsletter && {
-    name: 'newsletter',
-    href: `${config.newsletter}`,
-    title: `Newsletter ${config.author}`,
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M12 .64L8.23 3H5V5...' />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM.5 7h4v13h-4V7zm7.5 0h3.6v1.8h.05c.5-.9 1.7-1.85 3.5-1.85 3.75 0 4.45 2.5 4.45 5.75V20h-4v-6.2c0-1.5-.03-3.4-2.1-3.4-2.1 0-2.4 1.6-2.4 3.3V20h-4V7z"/>
       </svg>
     )
   },
@@ -62,34 +29,23 @@ const socialLinks: SocialLink[] = [
     href: `https://instagram.com/${config.instagram}`,
     title: `Instagram @${config.instagram}`,
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24">
-        <title>Instagram</title>
-        <path d="M12 2.2c3.2 0 ... (SVG truncated)" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9a5.5 5.5 0 0 1-5.5 5.5h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2zm0 2A3.5 3.5 0 0 0 4 7.5v9A3.5 3.5 0 0 0 7.5 20h9a3.5 3.5 0 0 0 3.5-3.5v-9A3.5 3.5 0 0 0 16.5 4h-9zm9.25 1.75a.75.75 0 1 1 0 1.5.75.75 0 0 1 0-1.5zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6z"/>
       </svg>
     )
   },
 
-  config.tiktok && {
-    name: 'tiktok',
-    href: `https://www.tiktok.com/@${config.tiktok}`,
-    title: `TikTok @${config.tiktok}`,
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
-        <path d="M232,72a72.1 ..." />
-      </svg>
-    )
-  },
+config.tiktok && {
+  name: 'tiktok',
+  href: `https://www.tiktok.com/@${config.tiktok}`,
+  title: `TikTok @${config.tiktok}`,
+  icon: (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor">
+      <path d="M232,72a72.1,72.1,0,0,1-47.3-17.7A71.7,71.7,0,0,1,160,17a8,8,0,0,0-8-9H128a8,8,0,0,0-8,8V152a24,24,0,1,1-24-24,8,8,0,0,0-8-8V96a8,8,0,0,0-8-8A56,56,0,1,0,136,200V88.9A104.6,104.6,0,0,0,168,104a103.1,103.1,0,0,0,64,22.1,8,8,0,0,0,8-8V80A8,8,0,0,0,232,72Z" />
+    </svg>
+  )
+}
 
-  config.youtube && {
-    name: 'youtube',
-    href: `https://www.youtube.com/${config.youtube}`,
-    title: `YouTube ${config.youtube}`,
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M10,15L15.19 ...' />
-      </svg>
-    )
-  }
 ].filter(Boolean)
 
 export function PageSocial() {
