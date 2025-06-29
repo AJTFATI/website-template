d2db4c56cc32f34ab82e470ffe216cfa666fc97a
AJTFATI <TAYLORALEXANDERJORDAN@GMAIL.COM>
2025-06-29 04:22:32 -0400

    Update IGShareButton.tsx

--- IGShareButton.tsx
+++ IGShareButton.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export function InstagramCorner() {
   return (
     <a
@@ -20,15 +22,16 @@ export function InstagramCorner() {
         alignItems: 'center',
         textDecoration: 'none',
         overflow: 'hidden',
+        transition: 'transform 0.3s ease-in-out',
+      }}
+      onMouseEnter={(e) => {
+        e.currentTarget.firstElementChild?.classList.add('flutter')
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.firstElementChild?.classList.remove('flutter')
       }}
     >
       <svg
-        width="32"
-        height="32"
-        viewBox="0 0 448 512"
-        fill="white"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-      >
-        <path d="M224,202.66A53.34,53.34,0,1,0,277.34,256,53.38,53.38,0,0,0,224,202.66Zm124.71-41a54,54,0,0,0-30.14-30.16C296.24,124,224,124,224,124s-72.24,0-94.57,7.5a54,54,0,0,0-30.14,30.16C91.79,149.76,88,177.87,88,2
+        cla
+
 
